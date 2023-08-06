a6ec99f36c8346cc31d00bdbcbf6a65f15235265
Kjetil Fjellheim <kjetil@forgottendonkey.net>
2023-08-06 02:04:44 +0200

    Implemented after and before arguments

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,17 @@ extern crate queues;
 
 use clap::{arg, command};
 use std::io::{BufReader, BufWriter, Write, Read };
-use std::io::prelude::*;
+use std::io::prelude::BufRead;
 use std::fs::File;
 use regex::Regex;
-use queues::CircularBuffer;
+use queues::{*};
 
-struct Arguments<'a> {
+struct Arguments {
     input: Box<dyn Read>,
     output: Box<dyn Write>,
     regexp: Vec<String>,
-    before_lines: &'a i32,
-    after_lines: &'a i32
+    before_lines: i32,
+    after_lines: i32
 }
 
 fn main() {
@@ -27,51 +27,52 @@ fn main() {
 fn setup() -> clap::ArgMatches {
     command!() // requires `cargo` feature
         .about("FIT file reader. Parse the results to either file or stdout. Will in future versions allow for filtering.")
-        .author("Kjetil Fjellheim <kjetil@forgottendonkey.net>")        
+        .author("Kjetil Fjellheim <kjetil@forgottendonkey.net>")
         .arg(
             arg!(
                 -i --input <FILE> "Input file"
-            )                
+            )
             .required(false)
         )
         .arg(
             arg!(
                 -o --output <FILE> "Output file"
-            )            
+            )
             .required(false)
         )
         .arg(
             arg!(
                 -r --regexp <REGEXP> "Regular expression to filter on"
-            )            
+            )
+            .action(clap::ArgAction::Append)
             .value_parser(clap::value_parser!(String))
             .required(false)
-        )   
+        )
         .arg(
             arg!(
-                -b --before <NUMN> "Number of lines to include before"
-            )            
+                -b --before <NUM> "Number of lines to include before"
+            )
             .value_parser(clap::value_parser!(i32))
             .default_missing_value("0")
             .required(false)
-        )         
+        )
         .arg(
             arg!(
-                -a --after <NUMN> "Number of lines to include after"
-            )            
+                -a --after <NUM> "Number of lines to include after"
+            )
             .value_parser(clap::value_parser!(i32))
             .default_missing_value("0")
             .required(false)
-        )                     
+        )
         .get_matches()
 }
 
-fn get_arguments<'a>(argument_matcher: clap::ArgMatches) -> Arguments<'a> {
+fn get_arguments<'a>(argument_matcher: clap::ArgMatches) -> Arguments {
     let input: Box<dyn Read> = get_input(&argument_matcher);
     let output: Box<dyn Write> = get_output(&argument_matcher);
     let regxp: Vec<String> = get_regexp(&argument_matcher);
-    let before_lines: &i32 = get_argument_value(&argument_matcher, "before");
-    let after_lines: &i32 = get_argument_value(&argument_matcher, "after");
+    let before_lines: i32 = get_argument_value(&argument_matcher, "before", 0);
+    let after_lines: i32 = get_argument_value(&argument_matcher, "after", 0);
 
     Arguments {
         input: input,
@@ -103,6 +104,7 @@ fn get_output(argument_matcher: &clap::ArgMatches) -> Box<dyn Write> {
 fn get_input(argument_matcher: &clap::ArgMatches) -> Box<dyn Read> {
     let mut input: Box<dyn Read> = Box::new(std::io::stdin());
     if let Some(input_path) = argument_matcher.get_one::<String>("input") {
+        println!("{}", input_path);
         let file_result = Box::new(File::open(input_path).unwrap());
         let reader = BufReader::new(file_result);
         input = Box::new(reader);
@@ -110,27 +112,35 @@ fn get_input(argument_matcher: &clap::ArgMatches) -> Box<dyn Read> {
     input
 }
 
-fn get_argument_value<'a, T: Clone + std::marker::Sync + std::marker::Send>(argument_matcher: &clap::ArgMatches, argument_name: &str) -> &'static T {
-    argument_matcher.get_one::<&'static T>(argument_name).expect("Invalid argument")
+fn get_argument_value(argument_matcher: &clap::ArgMatches, argument_name: &str, default: i32) -> i32 {
+   argument_matcher.get_one(argument_name).unwrap_or(&default).clone()
 }
 
 fn parse(arguments: Arguments) {
+    let mut before_buffer: CircularBuffer<String> = CircularBuffer::<String>::new(arguments.before_lines.clone() as usize);
+    let mut after_line: i32 = 0;
     let input = arguments.input;
     let mut output = arguments.output;
     let reader = BufReader::new(input);
     let regexps = arguments.regexp.into_iter().map(|r| Regex::new(&r).expect("Invalid regular expression")).collect::<Vec<Regex>>();
-    for line in reader.lines() {        
+    for line in reader.lines() {
         match line {
-            Ok(line) => {               
-                if is_match_any(&line, &regexps) {
-                    output.write_all(&line.as_bytes()).unwrap();
-                    output.write_all(b"\n").unwrap();
-                }               
+            Ok(line) => {
+                if after_line <= 0 && is_match_any(&line, &regexps) {
+                    output_before_lines(&mut before_buffer, &mut output);
+                    output_line(&line, &mut output);
+                    after_line = arguments.after_lines;
+                } else if after_line > 0 {
+                    output_line(&line, &mut output);
+                    after_line -= 1;
+                } else {
+                    let _ = before_buffer.add(line);
+                }
             },
             Err(e) => {
                 std::io::stderr().write_all(format!("Error reading line: {}", e).as_bytes()).unwrap();
             }
-        }                
+        }
     }
 }
 
@@ -141,4 +151,21 @@ fn is_match_any(line: &String, regexps: &Vec<Regex>) -> bool {
         }
     }
     false
+}
+
+fn output_before_lines(before_buffer: &mut CircularBuffer<String>, output: &mut Box<dyn Write>) {
+    while before_buffer.size() > 0 {
+        let before_line = before_buffer.remove();
+        match before_line {
+            Ok(before_line) => {
+                output_line(&before_line, output);
+            },
+            Err(_) => {}
+        }
+    }
+}
+
+fn output_line(line: &String, output: &mut Box<dyn Write>) {
+    output.write_all(&line.as_bytes()).unwrap();
+    output.write_all(b"\n").unwrap();
 }
